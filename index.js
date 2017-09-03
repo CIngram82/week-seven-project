48e180d49c53ad974b8eda8700ca5a601d4582c4
Christopher Ingram <cingram82@gmail.com>
2017-09-03 08:24:51 -0400

    most pages set up and some post/get working

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ server.use(bodyparse.urlencoded({
 server.use(express.static('public'));
 
 server.use(session({
-  secret: '',
+  secret: 'HI BEN!53wzcr7bgyhu',
   resave: false,
   saveUninitialized: true,
 }));
@@ -23,31 +23,21 @@ server.use(session({
 server.listen(3000, function() {
   console.log('Gabble ready for all the gabs');
 });
+
+
 ////// All Schemas
 const db = new Sequelize('gabble', 'Chris', '', {
   dialect: 'postgres',
 });
 
 const User = db.define('user', {
-  username: Sequelize.STRING, // Unique?
+  username: {type: Sequelize.STRING, unique: true}, // Unique?
   password: Sequelize.STRING, // need to add a hash to this ask ben or luke about it.
   displayName: Sequelize.STRING,
 });
 
 User.sync().then(function() {
   console.log("User BD online");
-
-  // User.create({
-  //   username: 'test',
-  //   password: 'test',
-  //   displayName: 'Test User',
-  // });
-
-  // User.create({
-  //   username: 'Cingram',
-  //   password: '123qwe',
-  //   displayName: 'Chris',
-  // });
 });
 
 const Messages = db.define('messages', {
@@ -58,10 +48,6 @@ const Messages = db.define('messages', {
 Messages.sync().then(function() {
   console.log("Messages BD online");
 
-  // Messages.create({
-  // text: 'The first ever gabble!',
-  // userId: '1', //check the userId for username: test
-  // });
 });
 
 const Likes = db.define('likes', {
@@ -71,37 +57,83 @@ const Likes = db.define('likes', {
 
 Likes.sync().then(function() {
   console.log("Likes BD online");
+});
+//////End of Schemas
+//////Start of get and post
 
-  // Likes.create({
-  // messagesId: '1', // check the messagesId for the first message.
-  // userId: Sequelize.INTEGER, // check the userId for Cingram.
-  // });
+server.get('/', function(req,res){
+  res.render('login');
 });
 
-//////
+server.get('/newGabble', function(req,res){
+  res.render('newGabble');
+});
 
+server.get('/gabInfo', function(req,res){
+  res.render('gabInfo');
+});
 
+server.post('/login', function(req,res){
+  User.findOne({ where:{
+    username : req.body.username,
+    password : req.body.password
+  }})
+  .then(function(user){
+    req.session.who = user;
+      res.redirect('home')
+      })
+  .catch(function(){
+    res.render('login');
+  });
+});
+
+server.get('/home', function(req,res){
+  Messages.findAll() // how to limit this to 10 or 20?
+  .then(function(messages){
+    res.render('home',{
+      userInfo : req.session.who,
+      allMessages : messages // how to set the userId to be the matching username?
+    });
+    })
+});
+
+server.get('/regNewUser', function(req,res){
+  res.render('regNewUser')
+});
+
+server.post('/newGabbleMessage', function(req,res){
+  Messages.create({
+    text: req.body.newGabbleText,
+    userId: req.session.who.id
+  })
+  .then(function(gabble){
+    res.render('home',{
+      userInfo: req.session.who,
+      allMessages : gabble
+    })
+  })
+})
 // Use Express and Sequelize to build a social network for students.
 //This network, called "Gabble," has users, public messages, and likes.
 //
 // Users should:
 //
-// have a username and password for logging in
-// have a display name
+// <>have a username and password for logging in
+// <>have a display name
+
 // Messages should:
-//
-// be associated with a user who wrote them
+// <>be associated with a user who wrote them
 // have up to 140 characters of text
+
 // Likes should:
-//
-// be associated with a user who made the like and a message that was liked
+// <>be associated with a user who made the like and a message that was liked
+
 // The application should have ways to do the following:
-//
-// Sign up as a new user
-// Log in
-// Log out
-// View all messages with the newest first
-// Create a new message
+// <>Sign up as a new user
+// <>Log in
+// <>Log out
+// <>View all messages with the newest first
+// <>Create a new message
 // Delete one of your own messages
 // Like a message
 // See who liked a message
